663d7654938fd8c91f7b057032c893c467e555cc
VanTuoi <vantuoi.dev@gmail.com>
2024-11-23 15:02:07 +0700

    feature: change info and password

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  updateUserProfile,
+  changePassword,
+} = require("../controllers/userController");
 
 // Đăng nhập
 router.post("/login", loginUser);
+// Cập nhật thông tin người dùng
+router.put("/:id", updateUserProfile);
 // Define the registration route
 router.post("/", registerUser);
+// Thay đổi mật khẩu
+router.put("/:id/change-password", changePassword);
 
 module.exports = router;
